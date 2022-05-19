0d9ddb5873479f6d45fdc7e64e22c87fc2ea5cd8
kentaro kimoto <exiler2411k@gmail.com>
2022-05-19 12:50:20 +0900

    init

---
+++ AggregateSNSApp/AggregateSNSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AggregateSNSApp
+//
+//  Created by 木元健太郎 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
